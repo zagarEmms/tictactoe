70f8e50547436dbeaf0ec79ee942be9a476c29f5
superfer2002 <fporcel2002@gmail.com>
2022-02-26 18:22:59 +0100

    declaration of all buttons and their logics

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -6,12 +6,10 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     private ImageButton box1;
     private ImageButton box2;
     private ImageButton box3;
@@ -22,15 +20,15 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton box8;
     private ImageButton box9;
 
-    private boolean click1;
-    private boolean click2;
-    private boolean click3;
-    private boolean click4;
-    private boolean click5;
-    private boolean click6;
-    private boolean click7;
-    private boolean click8;
-    private boolean click9;
+    private boolean click1 = true;
+    private boolean click2 = true;
+    private boolean click3 = true;
+    private boolean click4 = true;
+    private boolean click5 = true;
+    private boolean click6 = true;
+    private boolean click7 = true;
+    private boolean click8 = true;
+    private boolean click9 = true;
 
     private boolean player = true;
 
@@ -60,18 +58,162 @@ public class MainActivity extends AppCompatActivity {
         box1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //if (click1) {
+                if (click1) {
                     if (player) {
                         box1.setBackgroundResource(R.drawable.circle);
                     } else {
-                        box1.setBackgroundResource(R.drawable.cross);
+                        //box1.setImageResource(R.drawable.cross);
                     }
                     click1 = false;
                     setText();
                     changePlayer();
-                /*} else {
+                } else {
                     errorToast.show();
-                }*/
+                }
+            }
+        });
+        box2 = (ImageButton) findViewById(R.id.box2);
+        box2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (click2) {
+                    if (player) {
+                        box2.setBackgroundResource(R.drawable.circle);
+                    } else {
+                        box2.setImageResource(R.drawable.cross);
+                    }
+                    click2 = false;
+                    setText();
+                    changePlayer();
+                } else {
+                    errorToast.show();
+                }
+            }
+        });
+        box3 = (ImageButton) findViewById(R.id.box3);
+        box3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (click3) {
+                    if (player) {
+                        box3.setBackgroundResource(R.drawable.circle);
+                    } else {
+                        box3.setImageResource(R.drawable.cross);
+                    }
+                    click3 = false;
+                    setText();
+                    changePlayer();
+                } else {
+                    errorToast.show();
+                }
+            }
+        });
+        box4 = (ImageButton) findViewById(R.id.box4);
+        box4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (click4) {
+                    if (player) {
+                        box4.setBackgroundResource(R.drawable.circle);
+                    } else {
+                        box4.setImageResource(R.drawable.cross);
+                    }
+                    click4 = false;
+                    setText();
+                    changePlayer();
+                } else {
+                    errorToast.show();
+                }
+            }
+        });
+        box5 = (ImageButton) findViewById(R.id.box5);
+        box5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (click5) {
+                    if (player) {
+                        box5.setBackgroundResource(R.drawable.circle);
+                    } else {
+                        box5.setImageResource(R.drawable.cross);
+                    }
+                    click5 = false;
+                    setText();
+                    changePlayer();
+                } else {
+                    errorToast.show();
+                }
+            }
+        });
+        box6 = (ImageButton) findViewById(R.id.box6);
+        box6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (click6) {
+                    if (player) {
+                        box6.setBackgroundResource(R.drawable.circle);
+                    } else {
+                        box6.setImageResource(R.drawable.cross);
+                    }
+                    click6 = false;
+                    setText();
+                    changePlayer();
+                } else {
+                    errorToast.show();
+                }
+            }
+        });
+        box7 = (ImageButton) findViewById(R.id.box7);
+        box7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (click7) {
+                    if (player) {
+                        box7.setBackgroundResource(R.drawable.circle);
+                    } else {
+                        box7.setImageResource(R.drawable.cross);
+                    }
+                    click7 = false;
+                    setText();
+                    changePlayer();
+                } else {
+                    errorToast.show();
+                }
+            }
+        });
+        box8 = (ImageButton) findViewById(R.id.box8);
+        box8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (click8) {
+                    if (player) {
+                        box8.setBackgroundResource(R.drawable.circle);
+                    } else {
+                        box8.setImageResource(R.drawable.cross);
+                    }
+                    click8 = false;
+                    setText();
+                    changePlayer();
+                } else {
+                    errorToast.show();
+                }
+            }
+        });
+        box9 = (ImageButton) findViewById(R.id.box9);
+        box9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (click9) {
+                    if (player) {
+                        box9.setBackgroundResource(R.drawable.circle);
+                    } else {
+                        box9.setImageResource(R.drawable.cross);
+                    }
+                    click9 = false;
+                    setText();
+                    changePlayer();
+                } else {
+                    errorToast.show();
+                }
             }
         });
     }
@@ -79,8 +221,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        setButtons ();
         setText();
+        setButtons();
     }
 }
