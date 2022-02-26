f02d10d2bbdcbdbc37cec0c378cd99701025cc18
superfer2002 <fporcel2002@gmail.com>
2022-02-26 18:58:36 +0100

    correct players turns

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -220,7 +220,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        TextView textView = findViewById(R.id.playersTurn);
+        textView.setText(R.string.player1);
 
         setContentView(R.layout.activity_main);
         setText();
